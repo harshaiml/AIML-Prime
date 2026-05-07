1b481c204a9875681b93f8397cfde882f1e0f275
Harsh Vardhan Kushwaha <harshvardhanaiml@gmail.com>
2026-05-07 19:26:17 +0530

    Update file

--- SQL/Part-2/Accounts.sql
+++ SQL/Part-2/Accounts.sql
@@ -1,28 +1,30 @@
+-- 1. Database setup and Table creation
 CREATE DATABASE IF NOT EXISTS prime;
 USE prime;
 
 CREATE TABLE accounts (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50),
-    balance DECIMAL (10, 2)
+    balance DECIMAL(10, 2)
 );
 
+-- 2. Fresh Data Insert
 INSERT INTO accounts (name, balance) VALUES
-('Adam', 500.00),
-('Bob', 300.00),
-('Charlie',1000.00);
+('ADAM', 400.00),
+('BOb', 300.00),
+('Charlie', 1000.00);
 
+-- check after insert data
 SELECT * FROM accounts;
 
--- transactions
-
+-- 3. Transaction Logic with Savepoint
 START TRANSACTION;
 
 UPDATE accounts SET balance = balance + 1000 WHERE id = 1;
 SAVEPOINT after_wallet_topup;
 
 UPDATE accounts SET balance = balance + 10 WHERE id = 1;
--- error
+
 ROLLBACK TO after_wallet_topup;
 COMMIT;
 
