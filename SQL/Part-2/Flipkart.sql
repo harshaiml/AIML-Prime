c16cb940d72f859ce020dedf149bd6190626f8be
Harsh Vardhan Kushwaha <harshvardhanaiml@gmail.com>
2026-05-07 19:25:26 +0530

    Update file

--- SQL/Part-2/Flipkart.sql
+++ SQL/Part-2/Flipkart.sql
@@ -134,3 +134,69 @@ ON c.customer_id = o.customer_id;
 SELECT * FROM view2;
 
 DROP VIEW view1;
+
+-- Index in SQL
+-- Create new Table
+CREATE  TABLE accounts (
+    account_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    balance DECIMAL(10, 2),
+    branch VARCHAR(50)
+);
+
+INSERT INTO accounts VALUES
+(1, 'Adam', 500.00, 'Mumbai'),
+(2, 'Bob', 300.00, 'Delhi'),
+(3, 'Charlie', 700.00, 'Bangalore'),
+(4, 'David', 1000.00, 'Noida');
+
+SELECT * FROM accounts;
+
+CREATE INDEX idx_branch ON accounts(branch);
+
+SHOW INDEX FROM accounts;
+
+SELECT *
+FROM accounts
+WHERE branch = "Mumbai";
+
+CREATE INDEX idx2 ON accounts(branch, balance);
+SHOW INDEX FROM accounts;
+
+DROP INDEX idx2 ON accounts;
+
+-- Stored Procedures
+SELECT * FROM accounts;
+
+DELIMITER $$
+
+CREATE PROCEDURE check_balance(IN acc_id INT)
+BEGIN
+    SELECT balance
+    FROM accounts
+    WHERE account_id = acc_id;
+END $$
+
+DELIMITER $$
+
+CALL check_balance(2);
+
+-- ==================================================
+DROP PROCEDURE IF EXISTS check_balance;
+-- ==================================================
+
+SELECT * FROM accounts;
+
+DELIMITER $$
+
+CREATE PROCEDURE check_balance(IN acc_id INT, OUT bal DECIMAL(10, 2))
+BEGIN
+    SELECT balance INTO bal
+    FROM accounts
+    WHERE account_id = acc_id;
+END $$
+
+DELIMITER $$
+
+CALL check_balance(1, @balance);
+SELECT @balance;
